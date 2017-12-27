156bf05763045693264ab87d51f77802f653f3a5
Bill Hedworth <bill.hedworth@gmail.com>
2017-12-27 13:19:06 +0000

    add list topic sizes

--- src/clj_aiven/core.clj
+++ src/clj_aiven/core.clj
@@ -18,8 +18,9 @@
 (defn- get-http
   [{:keys [token]} url]
   (:body (http/get url
-                   {:headers {:Authorization (str "aivenv1 " token)}
-                    :as      :json})))
+                   {:headers          {:Authorization (str "aivenv1 " token)}
+                    :as               :json
+                    :throw-exceptions false})))
 
 (defn topic-info
   "Pulls back details of the specified topic."
@@ -33,8 +34,8 @@
     (get-http conn topic-endpoint)))
 
 #_(s/fdef topic-info
-        :args {:conn  ::connection
-               :topic ::topic})
+          :args {:conn  ::connection
+                 :topic ::topic})
 
 (defn- parse-topic-partition-offset
   [{:keys [consumer_groups] :as partition} group-name]
@@ -54,8 +55,38 @@
     {:partitions partitions
      :total-lag  (reduce + 0 (map :lag partitions))}))
 
+
+(defn get-subjects
+  [{:keys [url user pass] :as conn}]
+  (let [url  (format "%s/subjects" url)
+        resp (http/get url
+                       {:basic-auth [user pass]}
+                       :as :json)]
+    (-> resp
+        :body
+        (json/parse-string true))))
+
+
+(defn topic-size
+  [conn topic]
+  (let [topic-data (topic-info conn topic)
+        partitions (->> (get-in topic-data [:topic :partitions])
+                        (map (fn [partition]
+                               {:partition (:partition partition)
+                                :size      (:size partition)}))
+                        (filter some?))]
+    {:topic           topic
+     :retention_hours (get-in topic-data [:topic :retention_hours])
+     :total-size-KBs  (reduce + 0 (map :size partitions))}))
+
+(defn list-topic-sizes [conn schema-conn]
+  (doall (let [subjects (get-subjects schema-conn)
+               topics   (map #(clojure.string/replace % #"-value" "") subjects)]
+           (reverse (sort-by :total-size-KBs (map #(topic-size conn %) topics))))))
+
+
 #_(s/fdef topic-consumer-lag
-        :args {:conn       ::connection
-               :topic      ::topic
-               :group-name string?}
-        :ret (s/coll-of (s/keys :req-un [::total-lag ::partitions])))
+          :args {:conn       ::connection
+                 :topic      ::topic
+                 :group-name string?}
+          :ret (s/coll-of (s/keys :req-un [::total-lag ::partitions])))
