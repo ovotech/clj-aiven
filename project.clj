cc0c7944c9e4b3570b3fc34cd14472df22995732
Martino Visintin <martino.visintin@ovoenergy.com>
2018-02-15 12:03:40 +0000

    circleize

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/clj-aiven "0.3.0"
+(defproject ovotech/clj-aiven "0.3.1-SNAPSHOT"
   :description "Client lib for aiven api"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,4 +9,8 @@
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [overtone/at-at "1.2.0"]]
-  :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]]}})
+  :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]]}
+             :ci {:deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                                    :username      :env ;; LEIN_USERNAME
+                                                    :password      :env ;; LEIN_PASSWORD
+                                                    :sign-releases false}]]}})
