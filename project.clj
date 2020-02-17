71ce03019d36cd1f8c4ad3ec8a4c82a7fad2a50c
oli <oli.desborough@ovoenergy.com>
2020-02-17 16:58:51 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ovotech/clj-aiven "0.3.1-SNAPSHOT"
+(defproject ovotech/clj-aiven "0.4.0"
   :description "Client lib for aiven api"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
